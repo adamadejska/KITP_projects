f89b9a09b9bea14eeda531292a2cb8e71645b022
adamadejska <ada.alex.6@gmail.com>
2021-08-01 15:10:22 -0700

    Organization changes (make a function for the first task) + code for the both bacteria

--- Gutmicro/snps_day4.py
+++ Gutmicro/snps_day4.py
@@ -11,39 +11,57 @@ import bz2
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Read in the matrix
-filename = '/home/ada/Desktop/KITP_tutorials/kitp_2021_microbiome_data/snps/Alistipes_putredinis_61533/annotated_snps_small.txt.bz2'
 
-# The SNPs file is ~120 Mb big, might be better to read it without saving the whole matrix into the memory. 
-snps_file = bz2.BZ2File(filename,"r")
+def make_sfs(filename, samples_ids, name):
+    #This function makes Site-Frequency-Spectrum graphs given a SNP file and id numbers of samples 
 
-# First task: plot within-host Sire-Frequency-Spectrum for each type of bacteria samples
-line = str(snps_file.readline()) # header
-items = line.strip().split("\\t")
-samples = np.array([item.strip() for item in items[1:]])
+    # Read the file
+    for i in samples_ids:
 
-# Read the file
-samples_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]      # Samples we will make the SFSs for.
-for i in samples_ids:
-    mut_freq = []
-    for line in snps_file:
-        # Each value in the matrix is [Ai, Di] -> Ai = number of alt alleles at site i sample j, Di = depth for site i sample j
-        line = str(line)[:-3].split('\\t')    # Get rid of trailing \\n and separators \\t
-        info = line[0][2:]                    # ID | site i | gene ID | type of mutation (1D:nonsynonymous, 4D: synonymous) | ? | ?
-        
-        alt_num = float(line[i].split(',')[0])
-        depth = int(line[i].split(',')[1])
-        if depth == 0 or alt_num == 0:
-            continue
+        # The SNPs file is ~120 Mb big, might be better to read it without saving the whole matrix into the memory. 
+        snps_file = bz2.BZ2File(filename,"r")
 
-        # Save frequencies
-        mut_freq.append(alt_num/depth)
+        # First task: plot within-host Site-Frequency-Spectrum for each type of bacteria samples
+        line = str(snps_file.readline()) # header
+        items = line.strip().split("\\t")
+        samples = np.array([item.strip() for item in items[1:]])
+
+        mut_freq = []
+        for line in snps_file:
+            # Each value in the matrix is [Ai, Di] -> Ai = number of alt alleles at site i sample j, Di = depth for site i sample j
+            line = str(line)[:-3].split('\\t')    # Get rid of trailing \\n and separators \\t
+            info = line[0][2:]                    # ID | site i | gene ID | type of mutation (1D:nonsynonymous, 4D: synonymous) | ? | ?
+            
+            alt_num = float(line[i].split(',')[0])
+            depth = int(line[i].split(',')[1])
+            if depth == 0 or alt_num == 0:
+                continue
+
+            # Save frequencies
+            mut_freq.append(alt_num/depth)
 
         # Plot SFSs for each indicated sample (ignore values above 0.5 since those aren't minority alleles)
         bins = np.arange(0, 0.5, 0.01)
         plt.hist(mut_freq, bins=bins, edgecolor='black')
-        plt.title('SFS for sample ' + i + ' in Alistipes_putredinis_61533 file')
+        plt.title('SFS for sample ' + samples[i-1] + ' in ' + name + ' file')
         plt.xlabel('Alt frequency (alt num / depth) (bin size=0.01)')
         plt.ylabel('Frequency (log scale)')
         plt.yscale('log')
         plt.show()
+
+    return 
+
+"""
+# First, we make SFS graphs for Alistipes putredinis
+# Read in the matrix
+filename = '/home/ada/Desktop/KITP_tutorials/kitp_2021_microbiome_data/snps/Alistipes_putredinis_61533/annotated_snps_small.txt.bz2'
+samples_ids = range(1, 39, 1)      # Samples we will make the SFSs for.
+name = 'Alistipes_putredinis_61533'
+make_sfs(filename, samples_ids)
+"""
+
+# Next, make SFS graphs for the second bacteria  Bacteroides vulgatus
+filename = '/home/ada/Desktop/KITP_tutorials/kitp_2021_microbiome_data/snps/Bacteroides_vulgatus_57955/annotated_snps_small.txt.bz2'
+samples_ids = range(1, 39, 1)      # Samples we will make the SFSs for.
+name = 'Bacteroides_vulgatus_57955'
+make_sfs(filename, samples_ids, name)
